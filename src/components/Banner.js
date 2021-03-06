29eb44499b800edcb325c260dc1fd2ac67a5135f
Lorna Bogan <lorna.bogan@gmail.com>
2021-03-05 22:27:35 -0600

    buttons

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,23 +9,19 @@ function Banner() {
 
     return (
         <div className={"banner"}>
-            <img className="banner__title-img" alt="logo"></img>
-
+            <div className="banner__title-img" alt="logo"></div>
             <h1 className="banner__title">ANT Robotics</h1>
-           
-            <p className='banner__subtitle'>We simplify employment, team-up, and upgrade of transport robots in warehouses and factories.</p>
-           
-            <button 
-                className={`banner__video-button ${isOpen}`}
-                onClick={handleClick}>
-                    Watch Video
-            </button>
-
-            <button 
-                className={`banner__learn-button ${isOpen}`}
-                onClick={handleClick}>
-                    Learn More
-            </button>
+                <p className='banner__subtitle'>We simplify employment, team-up, and upgrade of transport robots in warehouses and factories.</p>
+                <button 
+                    className={`banner__video-button ${isOpen}`}
+                    onClick={handleClick}>
+                        Watch Video
+                </button>
+                <button 
+                    className={`banner__learn-button ${isOpen}`}
+                    onClick={handleClick}>
+                        Learn More
+                </button>
         </div>
     )
 }
