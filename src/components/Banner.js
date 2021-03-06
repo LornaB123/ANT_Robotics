da2009f34980c6b4e07d5e42c4b2f7dafbce46ed
Lorna Bogan <lorna.bogan@gmail.com>
2021-03-06 16:55:17 -0600

    added graphics

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,8 +9,17 @@ function Banner() {
 
     return (
         <div className={"banner"}>
-            <div className="banner__title-img" alt="logo"></div>
+            <img className= "banner__left-left-line" alt=""></img>
+            <img className= "banner__left-right-line" alt=""></img>
+            <img className= "banner__left-top-line" alt=""></img>
+
+            <div className="banner__title-img" alt="blocks"></div>
             <h1 className="banner__title">ANT Robotics</h1>
+
+            <img className= "banner__right-bottom-line" alt=""></img>
+            <img className= "banner__right-left-line" alt=""></img>
+            <img className= "banner__right-top-line" alt=""></img>
+
                 <p className='banner__subtitle'>We simplify employment, team-up, and upgrade of transport robots in warehouses and factories.</p>
                 <button 
                     className={`banner__video-button ${isOpen}`}
