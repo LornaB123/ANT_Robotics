ef7497c8624c7d4649ff4ad872959a1743dda506
Lorna Bogan <lorna.bogan@gmail.com>
2021-03-08 11:56:06 -0600

    implemented grid

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,31 +8,53 @@ function Banner() {
     }
 
     return (
-        <div className={"banner"}>
-            <img className= "banner__left-left-line" alt=""></img>
-            <img className= "banner__left-right-line" alt=""></img>
-            <img className= "banner__left-top-line" alt=""></img>
-
-            <div className="banner__title-img" alt="blocks"></div>
-            <h1 className="banner__title">ANT Robotics</h1>
-
-            <img className= "banner__right-bottom-line" alt=""></img>
-            <img className= "banner__right-left-line" alt=""></img>
-            <img className= "banner__right-top-line" alt=""></img>
-
-                <p className='banner__subtitle'>We simplify employment, team-up, and upgrade of transport robots in warehouses and factories.</p>
-                <button 
-                    className={`banner__video-button ${isOpen}`}
-                    onClick={handleClick}>
-                        Watch Video
-                </button>
-                <button 
-                    className={`banner__learn-button ${isOpen}`}
-                    onClick={handleClick}>
-                        Learn More
-                </button>
+      <div className={"banner"}>
+        <div className="banner__column-1">
+          <img className="banner__left-left-line" alt=""></img>
+          <img className="banner__left-left-square" alt=""></img>
+          <img className="banner__left-right-line" alt=""></img>
+          <img className="banner__left-right-square" alt=""></img>
+          <img className="banner__left-top-line" alt=""></img>
+          <img className="banner__left-top-square" alt=""></img>
         </div>
-    )
+
+        <div className="banner__column-2">
+          <div className="banner__title-img" alt="blocks"></div>
+          <img className="banner__orange-title-block" alt=""></img>
+          <img className="banner__title-image-top-square" alt=""></img>
+          <img className="banner__title-image-bottom-block" alt=""></img>
+
+          <h1 className="banner__title">ANT Robotics</h1>
+          <p className="banner__subtitle">
+            We simplify employment, team-up, and upgrade of transport robots in
+            warehouses and factories.
+          </p>
+          <div className="banner__button-container">
+          <button
+            className={`banner__video-button ${isOpen}`}
+            onClick={handleClick}
+          >
+            Watch Video
+          </button>
+          <button
+            className={`banner__learn-button ${isOpen}`}
+            onClick={handleClick}
+          >
+            Learn More
+          </button>
+          </div>
+        </div>
+
+        <div className="banner__column-3">
+          <img className="banner__right-bottom-line" alt=""></img>
+          <img className="banner__right-bottom-square" alt=""></img>
+          <img className="banner__right-left-line" alt=""></img>
+          <img className="banner__right-left-square" alt=""></img>
+          <img className="banner__right-top-line" alt=""></img>
+          <img className="banner__right-top-square" alt=""></img>
+        </div>
+      </div>
+    );
 }
 
 export default Banner;
