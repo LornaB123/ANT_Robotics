1fd6296f08da885d59bd965b57aabc3e9852a84d
Lorna Bogan <lorna.bogan@gmail.com>
2021-03-15 21:55:58 -0500

    images added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import NavBar from './NavBar'
 import Banner from './Banner'
 import Product from './Product'
+import Benefits from './Benefits'
 import data from '../utils/data.json'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Banner />
       <Product products={data.products} />
+      <Benefits />
     </div>
   );
 }
--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -3,24 +3,24 @@ function Benefits() {
       <div className="benefits">
           <h2 className="benefits__title"> Benefits </h2>
         <div className="benefits__column-1">
-            <div className="benefits_icon-1"></div>
-            <div />
-            <div />
+            <img className="benefits__image" alt="img" src="../images/benefits__img-1"></img>
+            <div className="benefits__column-title">Top-Notch Technology</div>
+            <div className="benefits__column-subtitle">Once you transform your indoor logistics, we'll keep it up-to-date.</div>
         </div>
         <div className="benefits__column-2">
-              <div />
-              <div />
-              <div />
+            <img className="benefits__image" alt="img" src="../images/benefits__img-2"></img>
+            <div className="benefits__column-title">Up-To-Date Software</div>
+            <div className="benefits__column-subtitle">You always get immediate software updates including new features and algorithms in background.</div>
             </div>
         <div className="benefits__column-3">
-            <div />
-            <div />
-            <div />
+            <img className="benefits__image" alt="img" src="../images/benefits__img-3"></img>
+            <div className="benefits__column-title">Seamless Fleet Upgrade</div>
+            <div className="benefits__column-subtitle">No need to stop the production to upgrade, extend, or replace the fleet. No hassle needed for the new units to get to work.</div>
         </div>
         <div className="benefits__column-4">
-            <div />
-            <div />
-            <div />
+            <img className="benefits__image" alt="img" src="../images/benefits__img-4"></img>
+            <div className="benefits__column-title">Incompatible Robots Team-Up</div>
+            <div className="benefits__column-subtitle">Robots produced by tens of different vendors can collaborate in the same workspace.</div>
         </div>
       </div>
     );
