4fc2649852d6e57649d8eb507493ac589264b2d0
Lorna Bogan <lorna.bogan@gmail.com>
2021-03-15 20:59:39 -0500

    benefits initial commit

---
+++ src/components/Benefits.js
@@ -0,0 +1,29 @@
+function Benefits() {
+    return (
+      <div className="benefits">
+          <h2 className="benefits__title"> Benefits </h2>
+        <div className="benefits__column-1">
+            <div className="benefits_icon-1"></div>
+            <div />
+            <div />
+        </div>
+        <div className="benefits__column-2">
+              <div />
+              <div />
+              <div />
+            </div>
+        <div className="benefits__column-3">
+            <div />
+            <div />
+            <div />
+        </div>
+        <div className="benefits__column-4">
+            <div />
+            <div />
+            <div />
+        </div>
+      </div>
+    );
+}
+
+export default Benefits;
