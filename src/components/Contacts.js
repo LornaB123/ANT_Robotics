872ab67af6ccaa82816648dcd5f1afac134b4381
Aasmi Maam <aasmi.maam@gmail.com>
2021-03-24 18:54:24 -0400

    Review

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,33 @@
 function Contacts() {
-    return (
-        <footer className="contacts">
+    return (            
+            <>     
+            <div className="contacts">
+                <div className="contacts__container">
+                    <div>
+                        <h1 className="contacts__title">Contacts</h1>
+                        <h3 className="contacts__address">Address: 8000 Edgewater Dr., Oakland, CA 94621, USA</h3>
+                        <h3 className="contacts__phone">Phone: +1 (650) 209-0-269</h3>
+                        <h3 className="contacts__email">Email: info@antrobotics.tech</h3>
+                        <a href="https://www.linkedin.com/company/antrobotics" className="contacts__linkedin">Linkedin</a>                        
+                    </div>                
+                    <div className="contacts__map">
+                        <iframe className="contacts__map-iframe" title="AntRobotics" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3155.106697228476!2d-122.20330978440808!3d37.74064102189519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f85bee5c2f57b%3A0x115b0ab943d303e6!2s8000%20Edgewater%20Dr%2C%20Oakland%2C%20CA%2094621!5e0!3m2!1sen!2sus!4v1615921701664!5m2!1sen!2sus" 
+                         frameBorder="0" allowFullScreen="" loading="lazy"></iframe>
+                    </div>
+                    <div>
+                    <h3 className="contacts__copyright">© ANT Robotics, 2020.</h3> 
+                    </div>
+                </div>
+            </div>
+            </>            
+    );
+
+  }
+  
+  export default Contacts;
+
+  {/* This is lorna's section
+             <footer className="contacts">
             <div className="contacts__wrapper">
             <h2 className="contacts__title">Contacts</h2>
             <ul className="contacts__list">
@@ -16,8 +43,4 @@ function Contacts() {
                 width="600" height="450" frameBorder="0" allowFullScreen="" loading="lazy"></iframe>
             </div>
             <div className="contacts__copyright">&#169; ANT Robotics, 2020.</div>
-        </footer>
-    );
-  }
-  
-  export default Contacts;
+        </footer> */}       
