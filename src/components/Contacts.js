69b8a0e1233a0b8722786e08079ce29b3c13f51e
Moddoo <>
2021-03-27 10:49:37 -0500

    completed

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,25 +1,34 @@
 function Contacts() {
-    return (            
-            <>     
-            <div className="contacts">
-                <div className="contacts__container">
-                    <div>
-                        <h1 className="contacts__title">Contacts</h1>
-                        <h3 className="contacts__address">Address: 8000 Edgewater Dr., Oakland, CA 94621, USA</h3>
-                        <h3 className="contacts__phone">Phone: +1 (650) 209-0-269</h3>
-                        <h3 className="contacts__email">Email: info@antrobotics.tech</h3>
-                        <a href="https://www.linkedin.com/company/antrobotics" className="contacts__linkedin">Linkedin</a>                        
-                    </div>                
-                    <div className="contacts__map">
-                        <iframe className="contacts__map-iframe" title="AntRobotics" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3155.106697228476!2d-122.20330978440808!3d37.74064102189519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f85bee5c2f57b%3A0x115b0ab943d303e6!2s8000%20Edgewater%20Dr%2C%20Oakland%2C%20CA%2094621!5e0!3m2!1sen!2sus!4v1615921701664!5m2!1sen!2sus" 
-                         frameBorder="0" allowFullScreen="" loading="lazy"></iframe>
-                    </div>
-                    <div>
-                    <h3 className="contacts__copyright">© ANT Robotics, 2020.</h3> 
-                    </div>
-                </div>
+    return (      
+        <div className="contacts">
+            <div className="contacts__container">
+                <div>
+                    <h2 className="contacts__title">Contacts</h2>
+                    <p className="contacts__text">
+                        Address: <span className="contacts__text-reg">8000 Edgewater Dr., Oakland, CA 94621, USA</span>
+                    </p>
+                    <p className="contacts__text">
+                        Phone: <span className="contacts__text-reg">+1 (650) 209-0-269</span>
+                    </p>
+                    <p className="contacts__text">
+                        Email: <span className="contacts__text-reg">info@antrobotics.tech</span>
+                    </p>
+                    <a 
+                        href="https://www.linkedin.com/company/antrobotics" 
+                        className="contacts__linkedin"
+                        target="_blank"
+                        >Linkedin</a>   
+                    <p className="contacts__copyright">© ANT Robotics, 2020.</p>                          
+                </div>                
+                {/* <div className="contacts__map"> */}
+                    <iframe className="contacts__map-iframe" title="AntRobotics" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3155.106697228476!2d-122.20330978440808!3d37.74064102189519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f85bee5c2f57b%3A0x115b0ab943d303e6!2s8000%20Edgewater%20Dr%2C%20Oakland%2C%20CA%2094621!5e0!3m2!1sen!2sus!4v1615921701664!5m2!1sen!2sus" 
+                    frameBorder="0" allowFullScreen="" loading="lazy"></iframe>
+                {/* </div> */}
+                {/* <div>
+                <h3 className="contacts__copyright">© ANT Robotics, 2020.</h3> 
+                </div> */}
             </div>
-            </>            
+        </div>         
     );
 
   }
